933f3b0f36d119d9604a76ea3ec1e698fdfef265
神威 <116956427+shenwei234@users.noreply.github.com>
2024-07-22 16:04:41 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '神威 の 站点监测',
+
+  // UptimeRobot API Keys
+  // 支持Monitor-Specific API Keys和Read-Only API Key，推荐使用Read-Only API Key，可填写多个
+  ApiKeys: [
+    'ur2611217-e78fe30822df919039c32515',
+  ],
+
+  // 日志天数，建议90
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '个人主页',
+      url: 'https://www.shenwei.us.kg/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.shenwei.us.kg'
+    },
+  ],
+};
